8c72ea69332480773569e54bc3ae24c7a53e751b
Davanesh <davaneshsaminathan335@gmail.com>
2025-10-10 23:47:22 +0530

    isCycle

--- com/dava/DSA/graph/UnDirectedGraph.java
+++ com/dava/DSA/graph/UnDirectedGraph.java
@@ -46,7 +46,44 @@ public class UnDirectedGraph {
         }
     }
 
-    public boolean isCycleBFS(ArrayList<ArrayList<Integer>> graph, int node) {
-        
+    public boolean isCycleBFS(ArrayList<ArrayList<Integer>> graph) {
+        int size = graph.size();
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[size];
+        for(int i = 0; i < size; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                queue.offer(new int[] {i, -1});
+                while(!queue.isEmpty()) {
+                    int[] curr = queue.poll();
+                    for(int neig : graph.get(curr[0])) {
+                        if(!visited[neig]) {
+                            visited[neig] = true;
+                            queue.offer(new int[] {neig, curr[0]});
+                        } else if(neig != curr[1]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }   return false;
+    }
+
+    public boolean isCycleDFS(ArrayList<ArrayList<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+        return isCycleDFShHelper(graph, visited, 0, -1);
+    }
+    private boolean isCycleDFShHelper(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int node, int parentNode) {
+        visited[node] = true;
+        for(int neig : graph.get(node)) {
+            if(!visited[neig]) {
+                if(isCycleDFShHelper(graph, visited, neig, node)) {
+                    return true;
+                }
+            } else if(neig != parentNode) {
+                return true;
+            }
+        }
+        return false;
     }
 }
